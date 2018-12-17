6d6ee2dea01d6bcde547741b40cae1dde053a858
Bruce Johnson <bruce@yogainternational.com>
2018-12-17 12:51:03 -0500

    added the four problems to be solved

---
+++ function-args-exersises.js
@@ -0,0 +1,23 @@
+//your code here
+//1. Create a function called returnArg that takes in an argument and returns it
+
+
+//2. Create a function called addArgPlusThree that takes in an argument and 
+//returns it's value plus 3
+
+
+addArgPlusThree(3); //Should return 6
+
+//3. Create a function called addTwoArgs that takes in two arguments
+//adds them together, and returns that value
+
+
+addTwoArgs(3, 3); //Should return 6
+
+
+//4. Create a function called addArgAndVar that takes in an argument and
+//returns the value of the argument plus value
+var value = 3;
+
+
+addArgAndVar(3); //Should return 6
