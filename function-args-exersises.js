235f9a352c5dffbb613388b3713b71789625564e
Bruce Johnson <bruce@yogainternational.com>
2018-12-31 16:45:17 -0500

    added mocha and chai tests

--- function-args-exersises.js
+++ function-args-exersises.js
@@ -4,20 +4,17 @@
 
 //2. Create a function called addArgPlusThree that takes in an argument and 
 //returns it's value plus 3
+//addArgPlusThree(3) -> would return 6
 
-
-addArgPlusThree(3); //Should return 6
 
 //3. Create a function called addTwoArgs that takes in two arguments
 //adds them together, and returns that value
+//addTwoArgs(5, 5) -> would return 10
 
-
-addTwoArgs(3, 3); //Should return 6
 
 
 //4. Create a function called addArgAndVar that takes in an argument and
 //returns the value of the argument plus value
+//addArgAndVar(6) -> would return 9
 var value = 3;
 
-
-addArgAndVar(3); //Should return 6
