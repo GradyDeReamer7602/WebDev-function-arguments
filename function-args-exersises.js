7605b7c040887a673fd3859667ed8cf90e3ef8e4
GradyDeReamer7602 <50172539+GradyDeReamer7602@users.noreply.github.com>
2019-05-08 15:37:33 -0400

    adds new changes to solutions

--- function-args-exersises.js
+++ function-args-exersises.js
@@ -1,37 +1,27 @@
 //your code here
 //1. Create a function called returnArg that takes in an argument and returns it
-function returnArg(arg){
-	var returnArg
-	return(arg)
+function returnArg(arg) {
+	return arg;
 }
-//2. Create a function called addArgPlusThree that takes in an argument and 
+//2. Create a function called addArgPlusThree that takes in an argument and
 //returns it's value plus 3
 //addArgPlusThree(3) -> would return 6
-function addArgPlusThree(arg){
-	var returnArg
-	return(arg + 3)
-
+function addArgPlusThree(arg) {
+	return arg + 3;
 }
- 
 
 //3. Create a function called addTwoArgs that takes in two arguments
 //adds them together, and returns that value
 //addTwoArgs(5, 5) -> would return 10
 
-function addTwoArgs(arg1, arg2){
-	var returnArgs
-	var arg1 = 5
-	var arg2 = 1
-	return(arg1 + arg2)
+function addTwoArgs(arg1, arg2) {
+	return arg1 + arg2;
 }
 
 //4. Create a function called addArgAndVar that takes in an argument and
 //returns the value of the argument plus value
 //addArgAndVar(6) -> would return 9
-var addArgAndVar = function(arg1){
-	var arg = 10
-	var value = 3
-	return(arg + value)
+var addArgAndVar = function(arg1) {
+	const value = 3;
+	return arg1 + value;
 }
-
-
